82b89950d20231b8d2474d0120cdfc6f9d7fe614
Ana Paula Gomes <apgomes88@gmail.com>
2021-03-17 20:21:38 +0100

    Remove opção não utilizada

--- scripts/check.py
+++ scripts/check.py
@@ -43,50 +43,34 @@ def read_items(items_file):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("action")
     parser.add_argument("dir")
     args = parser.parse_args()
 
-    if args.action == "check":
-        path = Path(args.dir)
-        all_files = path.glob("**/*.json")
-        for file in all_files:
-            if file.name.startswith("consulta") and file.name.endswith(".json"):
-                print("==============================================")
-                print(file.parent)  # cidade/ano/modalidade
-                try:
-                    items = read_items(file)
-                    exists = 0
-                    not_found = 0
-                    all_files_from_parent = list(
-                        filter(Path.is_file, file.parent.glob("**/*"))
-                    )
-                    for item in items:
-                        unit = normalize_text(item["unit"])
-                        category = normalize_text(item["category"])
-                        filename = normalize_text(item["filename"])
-                        item_path = f"{file.parent}/{unit}/{category}/{filename}"
-                        if Path(item_path).exists():
-                            exists += 1
-                        else:
-                            not_found += 1
-                    print(
-                        f"Arquivos da pasta: {len(all_files_from_parent)-1} - Itens do JSON: {len(items)}"  # noqa
-                    )
-                    print(f"Encontrados: {exists} - Não encontrados: {not_found}")
-                except Exception as e:
-                    print(f"error: {e}")
-    elif args.action == "metrics":
-        path = Path(args.dir)
-        all_files = path.glob("**/*.json")
-        for file in all_files:
-            if file.name.startswith("consulta") and file.name.endswith(".json"):
-                print("==============================================")
+    path = Path(args.dir)
+    all_files = path.glob("**/*.json")
+    for file in all_files:
+        if file.name.startswith("consulta") and file.name.endswith(".json"):
+            print("==============================================")
+            print(file.parent)  # cidade/ano/modalidade
+            try:
                 items = read_items(file)
-                print(f"{len(items)} {file}")
+                exists = 0
+                not_found = 0
                 all_files_from_parent = list(
                     filter(Path.is_file, file.parent.glob("**/*"))
                 )
-                # exclui o JSON atual
-                print(f"{len(all_files_from_parent)-1} {file.parent}")
-                assert len(items) == len(all_files_from_parent) - 1
+                for item in items:
+                    unit = normalize_text(item["unit"])
+                    category = normalize_text(item["category"])
+                    filename = normalize_text(item["filename"])
+                    item_path = f"{file.parent}/{unit}/{category}/{filename}"
+                    if Path(item_path).exists():
+                        exists += 1
+                    else:
+                        not_found += 1
+                print(
+                    f"Arquivos da pasta: {len(all_files_from_parent)-1} - Itens do JSON: {len(items)}"  # noqa
+                )
+                print(f"Encontrados: {exists} - Não encontrados: {not_found}")
+            except Exception as e:
+                print(f"error: {e}")
